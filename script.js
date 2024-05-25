78f197b483a72e46306dfb71c7e5afb45e8f010a
guidobenigni <guidogeza@gmail.com>
2024-05-25 21:09:47 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
+const scriptURL = 'https://script.google.com/macros/s/AKfycbztdCqq-lWyJa_eB9eHktr2eTkuf_3wT0Ae188IvhHMN160hQSlN7rpGQPznEMebfHoCw/exec'; // Replace with your deployed script URL
+const form = document.forms['registrationForm'];
+    
+form.addEventListener('submit', e => {
+    e.preventDefault();
+    fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+        .then(response => {
+            if (response.ok) {
+                alert('Anmeldung erfolgreich!');
+                form.reset(); // Reset the form after successful submission
+            } else {
+                alert('Es gab ein Problem mit der Anmeldung.');
+            }
+        })
+        .catch(error => console.error('Fehler!', error.message));
+});
+
+document.getElementById('togglePrivacy').addEventListener('click', function() {
+    var privacyPolicy = document.getElementById('privacyPolicy');
+    if (privacyPolicy.style.display === 'none' || privacyPolicy.style.display === '') {
+        privacyPolicy.style.display = 'block';
+    } else {
+        privacyPolicy.style.display = 'none';
+    }
+});
 
